3bd1368f5134fdb0c0cf678c198a84891f327f2f
KernelX-debug <ghericasas@gmail.com>
2026-03-22 16:24:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const audio = document.getElementById("audio")
+const playBtn = document.getElementById("playBtn")
+const progress = document.getElementById("progress")
+
+let playing = false
+
+playBtn.onclick = () => {
+
+if(!playing){
+
+audio.play()
+
+playBtn.innerHTML = '<i class="fa-solid fa-pause"></i>'
+
+}else{
+
+audio.pause()
+
+playBtn.innerHTML = '<i class="fa-solid fa-play"></i>'
+
+}
+
+playing = !playing
+
+}
+
+audio.addEventListener("timeupdate",()=>{
+
+const percent =
+(audio.currentTime / audio.duration) * 100
+
+progress.style.width = percent + "%"
+
+})
+
+
+const canvas = document.getElementById("matrix")
+const ctx = canvas.getContext("2d")
+
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+
+const letters="アイウエオカキクケコ0123456789"
+
+const fontSize=16
+
+const columns = canvas.width/fontSize
+
+const drops=[]
+
+for(let x=0;x<columns;x++)
+drops[x]=1
+
+function draw(){
+
+ctx.fillStyle="rgba(0,0,0,0.05)"
+ctx.fillRect(0,0,canvas.width,canvas.height)
+
+ctx.fillStyle="#00ffae"
+ctx.font=fontSize+"px monospace"
+
+for(let i=0;i<drops.length;i++){
+
+const text=
+letters[Math.floor(Math.random()*letters.length)]
+
+ctx.fillText(text,i*fontSize,drops[i]*fontSize)
+
+if(drops[i]*fontSize>canvas.height && Math.random()>0.975)
+drops[i]=0
+
+drops[i]++
+
+}
+
+}
+
+setInterval(draw,35)
